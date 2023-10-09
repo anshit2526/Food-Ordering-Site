c6770fbce47da2f0fc83b12ef3d3e188d3c0ab35
anshit2526 <anshitsiddharth2003@gmail.com>
2023-10-09 17:38:02 +0530

    Initial commit for today

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let wishlist = document.getElementById('wishlist')
+
+wishlist.addEventListener('click', function () {
+    alert('This feature will be added soon!');
+})
+
+let order_now = document.getElementById('order_now-button')
+
+order_now.addEventListener('click', function () {
+    alert('We are growing, so we will start ordering food soon.');
+})
